dbdecfcff4d9e3ad7117171c6a63eb5b46d0f175
6b72 <kastur@gmail.com>
2012-06-04 22:54:20 -0700

    Split up proto_helper into cpp and h

--- artificial_birds/proto_helper.cpp
+++ artificial_birds/proto_helper.cpp
@@ -1,9 +1,4 @@
-#ifndef PROTO_HELPER_H__
-#define PROTO_HELPER_H__
-
-#include "btBulletDynamicsCommon.h"
-#include "proto.pb.h"
-
+#include "proto_helper.h"
 btVector3 make_btVector3(const proto::Vector3d& proto_vector) {
 	return btVector3(proto_vector.x(), proto_vector.y(), proto_vector.z());
 }
@@ -13,5 +8,3 @@ void make_Vector3d(const btVector3& vector, proto::Vector3d* proto_vector) {
 	proto_vector->set_y(vector.y());
 	proto_vector->set_z(vector.z());
 }
-
-#endif
