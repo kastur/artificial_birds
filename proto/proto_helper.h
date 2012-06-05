fe6970b40c08b449939dd7ea3731b25057dc8817
6b72 <kastur@gmail.com>
2012-06-04 22:57:08 -0700

    Split up proto_helper into .cpp and .h, for real.

---
+++ proto/proto_helper.h
@@ -0,0 +1,11 @@
+#ifndef PROTO_HELPER_H__
+#define PROTO_HELPER_H__
+
+#include "btBulletDynamicsCommon.h"
+#include "proto.pb.h"
+
+btVector3 make_btVector3(const proto::Vector3d& proto_vector);
+
+void make_Vector3d(const btVector3& vector, proto::Vector3d* proto_vector);
+
+#endif
