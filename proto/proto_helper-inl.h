5a988c10356686d76cc9d3940a4314ffd8715353
6b72 <kastur@gmail.com>
2012-06-04 22:55:21 -0700

    Revert "Split up proto_helper into cpp and h"
    This reverts commit dbdecfcff4d9e3ad7117171c6a63eb5b46d0f175.

--- proto/proto_helper-inl.h
+++ proto/proto_helper-inl.h
@@ -1,4 +1,9 @@
-#include "proto_helper.h"
+#ifndef PROTO_HELPER_H__
+#define PROTO_HELPER_H__
+
+#include "btBulletDynamicsCommon.h"
+#include "proto.pb.h"
+
 btVector3 make_btVector3(const proto::Vector3d& proto_vector) {
 	return btVector3(proto_vector.x(), proto_vector.y(), proto_vector.z());
 }
@@ -8,3 +13,5 @@ void make_Vector3d(const btVector3& vector, proto::Vector3d* proto_vector) {
 	proto_vector->set_y(vector.y());
 	proto_vector->set_z(vector.z());
 }
+
+#endif
